55a7443b8d0d6248858c58d706120662472c7032
kyohan <smith.khalyl@gmail.com>
2023-12-27 11:37:51 -0600

    day1 - part 2

--- src/2023/01.c
+++ src/2023/01.c
@@ -7,13 +7,8 @@
 
 #include <years/year2023.h>
 
-#if TESTING
 #define TESTFILEP1 "../src/2023/test/day01"
 #define TESTFILEP2 "../src/2023/test/day01_2"
-#else
-#define TESTFILEP1 NULL
-#define TESTFILEP2 NULL
-#endif
 
 #define NUMSSIZE 9
 #define MAXNUMLEN 5
@@ -32,17 +27,12 @@ void search_for_num(char *, int *, bool);
 void day01_2023(const char * filename) {
     printf("-- Day 01 - 2023 --\n");
 
-    FILE * p1, * p2;
-    if(TESTFILEP1) p1 = fopen(TESTFILEP1, "r");
-    else p1 = fopen(filename, "r");
-
+    FILE * p1 = fopen((TESTING) ? TESTFILEP1 : filename, "r");
     if(p1 == NULL) exit(EXIT_FAILURE);
     printf("Part 1: %d\n", part1_012023(p1));
     fclose(p1);
 
-    if(TESTFILEP2) p2 = fopen(TESTFILEP2, "r");
-    else p2 = fopen(filename, "r");
-
+    FILE * p2 = fopen((TESTING) ? TESTFILEP2 : filename, "r");
     if(p2 == NULL) exit(EXIT_FAILURE);
     printf("Part 2: %d\n", part2_012023(p2));
     fclose(p2);
@@ -73,61 +63,66 @@ int part2_012023(FILE * fp) {
 
     int sum = 0;
     while(fgets(line, sizeof(line), fp) != 0) {
-        int len = strlen(line);
-        int inums[NUMSSIZE];
-        for(int i = 0; i < NUMSSIZE; i++) inums[i] = -1;
+        int lnums[NUMSSIZE];
+        for(int i = 0; i < NUMSSIZE; i++) lnums[i] = -1;
 
         for(int n = 0; n < NUMSSIZE; n++) {
             const char * found = strstr(line, nums[n]);
             if(found != NULL) {
-                inums[n] = found - line;
+                lnums[n] = found - line;
             }
-            printf("n: %d -- %s -- %d\n", n+1, nums[n], inums[n]);
         }
 
-        char rev[BUFFER_SIZE];
-        strncpy(rev, line, sizeof(rev));
-        strrev(rev);
-        printf("Reverse string: %s\n", rev);
+        int rnums[NUMSSIZE];
+        for(int i = 0; i < NUMSSIZE; i++) rnums[i] = -1;
 
-        char * lptr = strpbrk(line, "123456789");
-        char * rptr = strpbrk(rev, "123456789");
-        if(lptr) printf("Left digit found: %s\n", lptr);
-        else printf("Left digit not found\n");
+        for(int n = 0; n < NUMSSIZE; n++) {
+            const char * found = strstr(line, nums[n]);
+            int i = (found != NULL) ? found - line : -1;
+            while(found) {
+                found = strstr(found + 1, nums[n]);
+                i = (found != NULL) ? found - line : i;
+            }
 
-        if(rptr) printf("Right digit found: %s\n", rptr);
-        else printf("Right digit not found\n");
+            rnums[n] = i;
+        }
 
         int lefti = -1, righti = -1;
+        int min = INT_MAX, max = -1;
         for(int i = 0; i < NUMSSIZE; i++) {
-            if(lefti == -1 || lefti > inums[i]) {
-                lefti = (inums[i] == -1) ? -1 : i;
-                righti = (inums[i] == -1) ? -1 : i;
-            } else {
-                righti = (inums[i] == -1) ? -1 : i;
+            if(lnums[i] <= min && lnums[i] != -1) {
+                min = lnums[i];
+                lefti = i;
+            }
+
+            if(rnums[i] >= max && rnums[i] != -1) {
+                max = rnums[i];
+                righti = i;
             }
         }
 
         int lnum = 0;
         int rnum = 0;
 
+        char * lptr = strpbrk(line, "123456789");
         if(lptr != NULL) {
-            printf("Left pointer should be at index: %lu\n", lptr - line);
-            lnum = (lptr - line < lefti) ? lptr[0] - '0' : inums[lefti];
+            lnum = (lptr - line < min) ? lptr[0] - '0' : lefti + 1;
         } else {
-            lnum = inums[lefti];
-            printf("%d, %s\n", inums[lefti], nums[lefti]);
+            lnum = lefti + 1;
         }
 
-        if(rptr != NULL) {
-            printf("Right pointer should be at index: %lu\n", (len - (rptr - rev)));
-            rnum = (len - (rptr - rev) > righti) ? rptr[0] - '0' : inums[righti];
-        } else {
-            printf("%d: %d, %s\n", righti, inums[righti], nums[righti]);
-            rnum = inums[righti];
+        char * rptr = strpbrk(line, "123456789");
+        int r = (rptr) ? rptr - line : -1;
+        while(rptr) {
+            rptr = strpbrk(rptr + 1, "123456789");
+            r = (rptr) ? rptr - line : r;
         }
 
-        printf("Line: %s\nLeft: %d, Right: %d\n", line, lnum, rnum);
+        if(r >= 0) {
+            rnum = (r > max) ? line[r] - '0' : righti + 1;
+        } else {
+            rnum = righti + 1;
+        }
 
         int num = lnum * 10 + rnum;
         sum += num;
